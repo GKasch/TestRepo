d4643243c6c74dce441b1be749b9cbb3bd782ab3
GKasch <griffin.kasch@yotes.collegeofidaho.edu>
2026-01-21 10:57:51 -0700

    add weather to data

--- scripts/clean_data.r
+++ scripts/clean_data.r
@@ -4,4 +4,5 @@ library(nycflights13)
 
 flights_airline <- flights |>
   left_join(airlines, by = c("carrier" = "carrier")) |>
-  left_join(planes, by = c("tailnum" = "tailnum"))
+  left_join(planes, by = c("tailnum" = "tailnum")) |>
+  left_join(weather)
