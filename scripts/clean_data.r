6e445eeb8a6d26b743400b453ef02580b6f36a84
GKasch <griffin.kasch@yotes.collegeofidaho.edu>
2026-01-21 11:04:23 -0700

    count number of flights by airline

--- scripts/clean_data.r
+++ scripts/clean_data.r
@@ -4,4 +4,6 @@ library(nycflights13)
 
 flights_airline <- flights |>
   left_join(airlines, by = c("carrier" = "carrier")) |>
-  left_join(planes, by = c("tailnum" = "tailnum"))
+  left_join(planes, by = c("tailnum" = "tailnum")) |>
+  group_by(carrier) |>
+  summarize(n())
