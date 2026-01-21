be09516627e9cabffa6075d7d3977b016fbb1fbf
GKasch <griffin.kasch@yotes.collegeofidaho.edu>
2026-01-21 10:53:33 -0700

    Add planes to flight data

--- scripts/clean_data.r
+++ scripts/clean_data.r
@@ -3,4 +3,5 @@ library(tidyverse)
 library(nycflights13)
 
 flights_airline <- flights |>
-  left_join(airlines, by = c("carrier" = "carrier"))
+  left_join(airlines, by = c("carrier" = "carrier")) |>
+  left_join(planes, by = c("tailnum" = "tailnum"))
